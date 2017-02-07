fb955440ee737d39f3f15cf337e1bfc0ce398835
Yue Zhang <yue.jack.zhang@gmail.com>
2017-02-06 21:56:35 -0800

    more performance improvements

--- uncurl/state_estimation.py
+++ uncurl/state_estimation.py
@@ -25,22 +25,21 @@ def _create_w_objective(m, X):
         d = m.dot(w)+eps
         return np.sum(d - X*np.log(d))/genes
     def deriv(w):
-        # TODO: make this more efficient?
         #print 'eval_w_deriv'
         # derivative of objective wrt all elements of w
         # for w_{ij}, the derivative is... m_j1+...+m_jn sum over genes minus 
         # x_ij
         w2 = w.reshape((m.shape[1], X.shape[1]))
         d = m.dot(w2)+eps
-        deriv = np.zeros(w.shape)
+        #deriv = np.zeros(w.shape)
         temp = X/d
         m_sum = m.sum(0)
         m2 = m.T.dot(temp)
-        #deriv = m_sum - m.T.dot(1-temp)
-        for i in range(clusters):
-            for j in range(cells):
-                deriv[i*cells+j] = m_sum[i] - m2[i,j]
-        return deriv/genes
+        deriv = m_sum.reshape((clusters, 1)) - m2
+        #for i in range(clusters):
+        #    for j in range(cells):
+        #        deriv[i*cells+j] = m_sum[i] - m2[i,j]
+        return deriv.flatten()/genes
     return objective, deriv
 
 def _create_m_objective(w, X):
@@ -59,19 +58,18 @@ def _create_m_objective(w, X):
         d = m.dot(w)+eps
         return np.sum(d - X*np.log(d))/genes
     def deriv(m):
-        # TODO: make this way more efficient
         #print 'eval_m_deriv'
         m2 = m.reshape((X.shape[0], w.shape[0]))
         d = m2.dot(w)+eps
-        deriv = np.zeros(m.shape)
+        #deriv = np.zeros(m.shape)
         temp = X/d
         w_sum = w.sum(1)
         w2 = w.dot(temp.T)
-        #deriv = w_sum - w.T.dot(1-temp)
-        for i in range(genes):
-            for j in range(clusters):
-                deriv[i*clusters+j] = w_sum[j] - w2[j,i]
-        return deriv/genes
+        deriv = w_sum - w2.T
+        #for i in range(genes):
+        #    for j in range(clusters):
+        #        deriv[i*clusters+j] = w_sum[j] - w2[j,i]
+        return deriv.flatten()/genes
     return objective, deriv
 
 def _w_equality_constraints(cells, clusters, i):
@@ -90,20 +88,22 @@ def _create_w_constraints(cells, clusters):
     eq_constraints = [{'type': 'eq', 'fun': x, 'jac': y} for x,y in equality_constraint()]
     return tuple(eq_constraints)
 
-def poisson_estimate_state(data, clusters, init_means=None, init_weights=None, max_iters=10, tol=1e-4, disp=True):
+def poisson_estimate_state(data, clusters, init_means=None, init_weights=None, max_iters=10, tol=1e-4, disp=True, inner_max_iters=400):
     """
     Uses a Poisson Covex Mixture model to estimate cell states and
-    cell state mixing.
+    cell state mixing weights.
 
     Args:
         data (array): genes x cells
         clusters (int): number of mixture components
+
+    Optional args:
         init_means (array): initial centers - genes x clusters. Default: kmeans++ initializations
         init_weights (array): initial weights - clusters x cells. Default: random(0,1)
         max_iters (int): maximum number of iterations. Default: 10
-        tol (float): if both M and W change by less than tol, then the iteration
-            is stopped. Default: 1e-4
-        disp (bool): whether or not to display optimization parameters.
+        tol (float): if both M and W change by less than tol, then the iteration is stopped. Default: 1e-4
+        disp (bool): whether or not to display optimization parameters. Default: True
+        inner_max_iters (int): Number of iterations to run in the scipy minimizer for M and W. Default: 400
 
     Returns:
         two matrices, M and W: M is genes x clusters, W is clusters x cells.
@@ -129,7 +129,7 @@ def poisson_estimate_state(data, clusters, init_means=None, init_weights=None, m
         m_bounds = [(0, None) for x in m_init]
         # step 1: given M, estimate W
         w_objective, w_deriv = _create_w_objective(means, data)
-        w_res = minimize(w_objective, w_init, method='SLSQP', jac=w_deriv, bounds=w_bounds, constraints=w_constraints, options={'disp':disp, 'maxiter':100})
+        w_res = minimize(w_objective, w_init, method='SLSQP', jac=w_deriv, bounds=w_bounds, constraints=w_constraints, options={'disp':disp, 'maxiter':inner_max_iters})
         w_diff = np.sqrt(np.sum((w_res.x-w_init)**2))/w_init.size
         w_new = w_res.x.reshape((clusters, cells))
         w_init = w_res.x
@@ -137,7 +137,7 @@ def poisson_estimate_state(data, clusters, init_means=None, init_weights=None, m
         m_objective, m_deriv = _create_m_objective(w_new, data)
         # method could be 'L-BFGS-B' or 'SLSQP'... SLSQP gives a memory error...
         # or use TNC...
-        m_res = minimize(m_objective, m_init, method='L-BFGS-B', jac=m_deriv, bounds=m_bounds, options={'disp':disp, 'maxiter':200})
+        m_res = minimize(m_objective, m_init, method='L-BFGS-B', jac=m_deriv, bounds=m_bounds, options={'disp':disp, 'maxiter':inner_max_iters})
         m_diff = np.sqrt(np.sum((m_res.x-m_init)**2))/m_init.size
         m_new = m_res.x.reshape((genes, clusters))
         m_init = m_res.x
