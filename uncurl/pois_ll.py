2c4f630388ec29bd758ff06b59524a02f8a40532
Yue Zhang <yue.jack.zhang@gmail.com>
2017-05-26 22:10:28 -0700

    added zip_ll

--- uncurl/pois_ll.py
+++ uncurl/pois_ll.py
@@ -40,3 +40,31 @@ def poisson_dist(p1, p2):
     p1 += 1e-10
     p2 += 1e-10
     return np.dot(p1-p2, np.log(p1/p2))
+
+# TODO: poisson_ll_zip
+
+def zip_ll(data, means, L):
+    """
+    Calculates the zero-inflated Poisson log-likelihood.
+
+    Args:
+        data (array): genes x cells
+        means (array): genes x k
+        L (array): genes x k - this is the probability of having a count of zero.
+
+    Returns:
+        cells x k array of log-likelihood for each cell/cluster pair.
+    """
+    genes, cells = data.shape
+    clusters = means.shape[1]
+    ll = np.zeros((cells, clusters))
+    d0 = (data==0)
+    d1 = (data>0)
+    for i in range(clusters):
+        means_i = np.tile(means[:,i], (cells, 1))
+        means_i = means_i.transpose()
+        L_i = np.tile(L[:,i], (cells, 1))
+        L_i = L_i.transpose()
+        ll[:,i] = np.sum(d0*np.log(L_i + (1 - L_i)*np.exp(-means_i)), 0)
+        ll[:,i] += np.sum(d1*(np.log(1 - L_i) + xlogy(data, means_i) - gammaln(data+1) - means_i), 0)
+    return ll
