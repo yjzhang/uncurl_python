0abd1c45cffe690b31e1675c02761fa2201ed139
Yue Zhang <yue.jack.zhang@gmail.com>
2017-07-20 22:20:37 -0700

    added zip state estimation

--- uncurl/__init__.py
+++ uncurl/__init__.py
@@ -3,6 +3,7 @@ from pois_ll import poisson_ll, poisson_dist
 from qual2quant import qualNorm
 from state_estimation import poisson_estimate_state
 from nb_state_estimation import nb_estimate_state
+from zip_state_estimation import zip_estimate_state
 from dim_reduce import dim_reduce, dim_reduce_data
 from lineage import lineage, pseudotime
 from nb_cluster import nb_cluster
