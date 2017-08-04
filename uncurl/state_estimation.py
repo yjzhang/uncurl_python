84a918aac49f3171f116f65fbe449c3b977899eb
Yue Zhang <yue.jack.zhang@gmail.com>
2017-08-03 17:57:09 -0700

    implemented nolips for poisson optimization

--- uncurl/state_estimation.py
+++ uncurl/state_estimation.py
@@ -17,6 +17,7 @@ def _create_w_objective(m, X):
     """
     genes, clusters = m.shape
     cells = X.shape[1]
+    m_sum = m.sum(0)
     def objective(w):
         # convert w into a matrix first... because it's a vector for
         # optimization purposes
@@ -26,7 +27,6 @@ def _create_w_objective(m, X):
         # for w_{ij}, the derivative is... m_j1+...+m_jn sum over genes minus 
         # x_ij
         temp = X/d
-        m_sum = m.sum(0)
         m2 = m.T.dot(temp)
         deriv = m_sum.reshape((clusters, 1)) - m2
         return np.sum(d - X*np.log(d))/genes, deriv.flatten()/genes
@@ -42,23 +42,48 @@ def _create_m_objective(w, X):
     """
     clusters, cells = w.shape
     genes = X.shape[0]
+    w_sum = w.sum(1)
     def objective(m):
         m = m.reshape((X.shape[0], w.shape[0]))
         d = m.dot(w)+eps
         temp = X/d
-        w_sum = w.sum(1)
         w2 = w.dot(temp.T)
         deriv = w_sum - w2.T
         return np.sum(d - X*np.log(d))/genes, deriv.flatten()/genes
     return objective
 
-def initialize_from_assignments(assignments, k):
+# TODO: nolips
+
+def nolips_update_w(X, M, W, disp=False):
+    """
+    Iteratively runs nolips updates.
+    """
+    genes, cells = X.shape
+    k = W.shape[0]
+    MW = M.dot(W)
+    W_new = np.copy(W)
+    R = M.sum(0)
+    for i in range(cells):
+        if disp:
+            print('cell: {0}'.format(i))
+        eta = 1./(2*X[:,i].sum())
+        w = W[:,i]
+        x = np.tile(X[:,i], (k,1)).T
+        mw = np.tile(MW[:,i], (k,1)).T
+        y2 = M*x
+        C = np.sum(y2/mw, 0)
+        p = w/(1 + eta*w*(R - C))
+        W_new[:,i] = np.max(np.array([np.zeros(k), p]), 0)
+    return W_new
+
+def initialize_from_assignments(assignments, k, max_assign_weight=0.75):
     """
     Creates a weight initialization matrix from Poisson clustering assignments.
 
     Args:
         assignments (array): 1D array of integers, of length cells
         k (int): number of states/clusters
+        max_assign_weight (float, optional): between 0 and 1 - how much weight to assign to the highest cluster. Default: 0.75
 
     Returns:
         init_W (array): k x cells
@@ -68,14 +93,14 @@ def initialize_from_assignments(assignments, k):
     for i, a in enumerate(assignments):
         # entirely arbitrary... maybe it would be better to scale
         # the weights based on k?
-        init_W[a, i] = 0.75
+        init_W[a, i] = max_assign_weight
         for a2 in range(k):
             if a2!=a:
-                init_W[a2, i] = 0.25/(k-1)
+                init_W[a2, i] = max_assign_weight/(k-1)
     return init_W
 
 # TODO: add reps - number of starting points
-def poisson_estimate_state(data, clusters, init_means=None, init_weights=None, max_iters=10, tol=1e-4, disp=True, inner_max_iters=400, reps=1, normalize=True):
+def poisson_estimate_state(data, clusters, init_means=None, init_weights=None, max_iters=10, tol=1e-4, disp=True, inner_max_iters=25, reps=1, normalize=True):
     """
     Uses a Poisson Covex Mixture model to estimate cell states and
     cell state mixing weights.
@@ -103,38 +128,47 @@ def poisson_estimate_state(data, clusters, init_means=None, init_weights=None, m
     else:
         means = init_means.copy()
     clusters = means.shape[1]
-    w_init = np.random.random(cells*clusters)
+    w_init = np.random.random((clusters,cells))
     if init_weights is not None:
         if len(init_weights.shape)==1:
             init_weights = initialize_from_assignments(init_weights, clusters)
-        w_init = init_weights.reshape(cells*clusters)
+        w_init = init_weights.copy()
     m_init = means.reshape(genes*clusters)
     # repeat steps 1 and 2 until convergence:
     ll = np.inf
+    # arbitrary argument
+    nolips_iters = inner_max_iters
     for i in range(max_iters):
         if disp:
             print('iter: {0}'.format(i))
-        w_bounds = [(0, None) for x in w_init]
-        m_bounds = [(0, None) for x in m_init]
         # step 1: given M, estimate W
         w_objective = _create_w_objective(means, data)
         # TODO: select between L-BFGS-B or SLSQP optimization methods
-        w_res = minimize(w_objective, w_init, method='L-BFGS-B', jac=True, bounds=w_bounds, options={'disp':disp, 'maxiter':inner_max_iters})
-        w_diff = np.sqrt(np.sum((w_res.x-w_init)**2))/w_init.size
-        w_new = w_res.x.reshape((clusters, cells))
-        w_init = w_res.x
+        #w_res = minimize(w_objective, w_init, method='L-BFGS-B', jac=True, bounds=w_bounds, options={'disp':disp, 'maxiter':inner_max_iters})
+        #w_diff = np.sqrt(np.sum((w_res.x-w_init)**2))/w_init.size
+        for i in range(nolips_iters):
+            w_new = nolips_update_w(data+eps, means, w_init)
+            #w_new = w_res.x.reshape((clusters, cells))
+            #w_new = w_new/w_new.sum(0)
+            w_init = w_new
+        w_ll, w_deriv = w_objective(w_new.reshape(clusters*cells))
+        if disp:
+            print('Finished updating W. Objective value: {0}'.format(w_ll))
         # step 2: given W, update M
+        for i in range(nolips_iters):
+            m_new = nolips_update_w(data.T+eps, w_new.T, means.T)
+            means = m_new.T
         m_objective = _create_m_objective(w_new, data)
+        m_ll, m_deriv = m_objective(means.reshape(genes*clusters))
+        if disp:
+            print('Finished updating M. Objective value: {0}'.format(m_ll))
         # method could be 'L-BFGS-B' or 'SLSQP'... SLSQP gives a memory error...
         # or use TNC...
-        m_res = minimize(m_objective, m_init, method='L-BFGS-B', jac=True, bounds=m_bounds, options={'disp':disp, 'maxiter':inner_max_iters})
-        ll = m_res.fun
-        m_diff = np.sqrt(np.sum((m_res.x-m_init)**2))/m_init.size
-        m_new = m_res.x.reshape((genes, clusters))
-        m_init = m_res.x
-        means = m_new
-        if w_diff < tol and m_diff < tol:
-            break
+        #m_res = minimize(m_objective, m_init, method='L-BFGS-B', jac=True, bounds=m_bounds, options={'disp':disp, 'maxiter':inner_max_iters})
+        #ll = m_res.fun
+        #m_diff = np.sqrt(np.sum((m_res.x-m_init)**2))/m_init.size
+        #if w_diff < tol and m_diff < tol:
+        #    break
     if normalize:
         w_new = w_new/w_new.sum(0)
-    return m_new, w_new, ll
+    return means, w_new, ll
