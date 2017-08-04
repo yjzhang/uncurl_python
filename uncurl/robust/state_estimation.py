217a05735e8791bdc959564a594465b5e00b10c3
Yue Zhang <yue.jack.zhang@gmail.com>
2017-08-03 18:10:47 -0700

    robust uncurl now uses nolips

--- uncurl/robust/state_estimation.py
+++ uncurl/robust/state_estimation.py
@@ -1,6 +1,7 @@
 # state estimation with poisson convex mixture model
 
 from uncurl.clustering import kmeans_pp
+from uncurl.state_estimation import initialize_from_assignments, nolips_update_w
 
 import numpy as np
 from scipy.optimize import minimize
@@ -16,10 +17,12 @@ def _create_poiss_w_objective(m, X):
     Args:
         m (array): genes x clusters
         X (array): genes x cells
-        selected_genes (array): array of ints - genes to be selected
     """
     # TODO: excluded genes
     genes, clusters = m.shape
+    m_sum = m.sum(0)
+    ms2 = m_sum.reshape((clusters, 1))
+    const = gammaln(X+1)
     def objective(w):
         # convert w into a matrix first... because it's a vector for
         # optimization purposes
@@ -29,10 +32,9 @@ def _create_poiss_w_objective(m, X):
         # for w_{ij}, the derivative is... m_j1+...+m_jn sum over genes minus 
         # x_ij
         temp = X/d
-        m_sum = m.sum(0)
         m2 = m.T.dot(temp)
-        deriv = m_sum.reshape((clusters, 1)) - m2
-        return np.sum(d - X*np.log(d))/genes, deriv.flatten()/genes
+        deriv = ms2 - m2
+        return np.sum(d - X*np.log(d) + const)/genes, deriv.flatten()/genes
     return objective
 
 def _create_poiss_m_objective(w, X):
@@ -42,18 +44,20 @@ def _create_poiss_m_objective(w, X):
     Args:
         w (array): clusters x cells
         X (array): genes x cells
-        selected_genes (array): array of ints - genes to be selected
+        included_genes (array): list of included genes
     """
     clusters, cells = w.shape
     genes = X.shape[0]
+    w_sum = w.sum(1)
+    const = gammaln(X+1)
     def objective(m):
-        m = m.reshape((X.shape[0], w.shape[0]))
+        m = m.reshape((genes, clusters))
         d = m.dot(w)+eps
+        s = d - X*np.log(d) + const
         temp = X/d
-        w_sum = w.sum(1)
         w2 = w.dot(temp.T)
         deriv = w_sum - w2.T
-        return np.sum(d - X*np.log(d))/genes, deriv.flatten()/genes
+        return np.sum(s)/genes, deriv.flatten()/genes
     return objective
 
 def _poisson_calculate_lls(X, M, W, use_constant=True, add_eps=True):
@@ -74,33 +78,10 @@ def _poisson_calculate_lls(X, M, W, use_constant=True, add_eps=True):
     L[np.isnan(L)] = -np.inf
     return L
 
-def initialize_from_assignments(assignments, k):
-    """
-    Creates a weight initialization matrix from Poisson clustering assignments.
-
-    Args:
-        assignments (array): 1D array of integers, of length cells
-        k (int): number of states/clusters
-
-    Returns:
-        init_W (array): k x cells
-    """
-    cells = len(assignments)
-    init_W = np.zeros((k, cells))
-    for i, a in enumerate(assignments):
-        # entirely arbitrary... maybe it would be better to scale
-        # the weights based on k?
-        init_W[a, i] = 0.75
-        for a2 in range(k):
-            if a2!=a:
-                init_W[a2, i] = 0.25/(k-1)
-    return init_W
-
 def one_round(data, M, W, selected_genes):
     pass
 
-# TODO: add hard thresholding
-def robust_estimate_state(data, clusters, dist='Poiss', init_means=None, init_weights=None, max_iters=10, tol=1e-4, disp=True, inner_max_iters=400, reps=1, normalize=True, gene_portion=0.2, use_constant=True):
+def robust_estimate_state(data, clusters, dist='Poiss', init_means=None, init_weights=None, max_iters=10, tol=1e-4, disp=True, inner_max_iters=25, reps=1, normalize=True, gene_portion=0.2, use_constant=True):
     """
     Uses a Poisson Covex Mixture model to estimate cell states and
     cell state mixing weights.
@@ -115,7 +96,6 @@ def robust_estimate_state(data, clusters, dist='Poiss', init_means=None, init_we
         tol (float, optional): if both M and W change by less than tol, then the iteration is stopped. Default: 1e-4
         disp (bool, optional): whether or not to display optimization parameters. Default: True
         inner_max_iters (int, optional): Number of iterations to run in the scipy minimizer for M and W. Default: 400
-        reps (int, optional): number of random initializations. Default: 1.
         normalize (bool, optional): True if the resulting W should sum to 1 for each cell. Default: True.
         gene_portion (float, optional): The proportion of genes to use for estimating W after hard thresholding. Default: 0.2
 
@@ -131,12 +111,10 @@ def robust_estimate_state(data, clusters, dist='Poiss', init_means=None, init_we
     else:
         means = init_means.copy()
     clusters = means.shape[1]
-    w_init = np.random.random(cells*clusters)
+    w_init = np.random.random((clusters, cells))
     if init_weights is not None:
         if len(init_weights.shape)==1:
             init_weights = initialize_from_assignments(init_weights, clusters)
-        w_init = init_weights.reshape(cells*clusters)
-    m_init = means.reshape(genes*clusters)
     # repeat steps 1 and 2 until convergence:
     ll = np.inf
     # objective functions...
@@ -145,38 +123,48 @@ def robust_estimate_state(data, clusters, dist='Poiss', init_means=None, init_we
     ll_func = _poisson_calculate_lls
     included_genes = np.arange(genes)
     num_genes = int(np.ceil(gene_portion*genes))
+    if disp:
+        print('num_genes: {0}'.format(num_genes))
+    nolips_iters = 25
     for i in range(max_iters):
         if disp:
             print('iter: {0}'.format(i))
         w_bounds = [(0, None) for x in w_init]
-        m_bounds = [(0, None) for x in m_init]
         # step 1: given M, estimate W
         w_objective = w_obj(means[included_genes,:], data[included_genes,:])
-        # TODO: select between L-BFGS-B or SLSQP optimization methods
-        w_res = minimize(w_objective, w_init, method='L-BFGS-B', jac=True, bounds=w_bounds, options={'disp':disp, 'maxiter':inner_max_iters})
-        w_diff = np.sqrt(np.sum((w_res.x-w_init)**2))/w_init.size
-        w_new = w_res.x.reshape((clusters, cells))
-        w_init = w_res.x
+        # convert to nolips
+        for i in range(nolips_iters):
+            w_new = nolips_update_w(data[included_genes,:]+eps, means[included_genes,:], w_init)
+            #w_new = w_res.x.reshape((clusters, cells))
+            #w_new = w_new/w_new.sum(0)
+            w_init = w_new
+        w_ll, w_deriv = w_objective(w_new.reshape(clusters*cells))
+        #w_res = minimize(w_objective, w_init, method='L-BFGS-B', jac=True, bounds=w_bounds, options={'disp':disp, 'maxiter':inner_max_iters})
+        #w_diff = np.sqrt(np.sum((w_res.x-w_init)**2))/w_init.size
+        #w_init = w_res.x
+        #w_new = w_res.x.reshape((clusters, cells))
         # step 2: given W, update M
-        m_objective = m_obj(w_new, data)
-        # method could be 'L-BFGS-B' or 'SLSQP'... SLSQP gives a memory error...
-        # or use TNC...
-        m_res = minimize(m_objective, m_init, method='L-BFGS-B', jac=True, bounds=m_bounds, options={'disp':disp, 'maxiter':inner_max_iters})
-        ll = m_res.fun
-        m_diff = np.sqrt(np.sum((m_res.x-m_init)**2))/m_init.size
-        m_new = m_res.x.reshape((genes, clusters))
-        m_init = m_res.x
-        means = m_new
-        if w_diff < tol and m_diff < tol:
-            break
+        w_ll, w_deriv = w_objective(w_new.reshape(clusters*cells))
+        if disp:
+            print('Finished updating W. Objective value: {0}'.format(w_ll))
+        # step 2: given W, update M
+        for i in range(nolips_iters):
+            m_new = nolips_update_w(data[included_genes,:].T+eps, w_new.T, means[included_genes,:].T)
+            means[included_genes,:] = m_new.T
+        m_objective = m_obj(w_new, data[included_genes,:])
+        m_ll, m_deriv = m_objective(means.reshape(genes*clusters))
+        if disp:
+            print('Finished updating M. Objective value: {0}'.format(m_ll))
+
         # step 3: hard thresholding/gene subset selection
-        lls = ll_func(data, m_new, w_new, use_constant)
+        lls = ll_func(data, means, w_new, use_constant)
         if i < max_iters - 1:
             included_genes = lls.argsort()[::-1][:num_genes]
             if disp:
                 print(lls[included_genes])
                 print(sum(~np.isinf(lls)))
                 print(included_genes)
+                print('selected number of genes: ' + str(len(included_genes)))
     if normalize:
         w_new = w_new/w_new.sum(0)
-    return m_new, w_new, ll, included_genes
+    return means, w_new, ll, included_genes
