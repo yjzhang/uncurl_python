1ea9c047ec34c7e43ae821095ae7eb4850378567
Yue Zhang <yjzhang@cs.washington.edu>
2018-10-10 18:02:06 -0700

    test gap score

--- tests/test_gap_score.py
+++ tests/test_gap_score.py
@@ -19,7 +19,7 @@ class GapScoreTest(TestCase):
     def test_gap_score(self):
         data_mat = scipy.io.loadmat('data/10x_pooled_400.mat')
         data = data_mat['data']
-        data_tsvd = gap_score.preproc_data(data)
+        data_tsvd = gap_score.preproc_data(data, gene_subset=True)
         max_k, gap_vals, sk_vals = gap_score.run_gap_k_selection(data_tsvd,
                 k_min=1, k_max=50, skip=5, B=5)
         # just test that the score is in a very broad range
@@ -29,7 +29,7 @@ class GapScoreTest(TestCase):
     def test_gap_score_2(self):
         data_mat = scipy.io.loadmat('data/GSE60361_dat.mat')
         data = data_mat['Dat']
-        data_tsvd = gap_score.preproc_data(data)
+        data_tsvd = gap_score.preproc_data(data, gene_subset=True)
         max_k, gap_vals, sk_vals = gap_score.run_gap_k_selection(data_tsvd,
                 k_min=1, k_max=50, skip=5, B=5)
         self.assertTrue(max_k > 3)
